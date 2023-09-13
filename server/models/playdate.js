e2fbe727d520828c0cc649829a0befbb16a208d2
Milena Maćkowiak <milenam@student.chalmers.se>
2023-09-13 17:53:05 +0200

    Schema for a playdate added

---
+++ server/models/playdate.js
@@ -0,0 +1,15 @@
+const playdateSchema = new mongoose.Schema({
+  location: { type: String },
+  time: { type: Time },
+  numberOfAttendees: { type: Number },
+  dog: {
+    type: mongoose.Schema.Types.Id,
+    ref: "Dog",
+  },
+  dogsitter: {
+    type: mongoose.Schema.Types.Id,
+    ref: "Dogsitter",
+  },
+});
+
+const Playdate = mongoose.model("Playdate", playdateSchema);
