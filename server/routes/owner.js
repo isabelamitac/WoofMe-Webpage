247fd18419fd54a1b8d0fa68bf04343ecee20acd
Anna <annamak@student.chalmers.se>
2023-09-24 17:59:50 +0200

    Add dog routes to owner

--- server/routes/owner.js
+++ server/routes/owner.js
@@ -9,7 +9,14 @@ const  {
 } = require('../controllers/ownerController.js');
 
 const  { 
-   createDog
+   createDog,
+   getAllDogs,
+   getDogById,
+   updateDog,
+   updateDogAge,
+   updateDogDiet,
+   deleteDogById,
+   deleteAllDogs
 } = require('../controllers/dogController.js');
 
 router.post('/', createOwner);
@@ -17,5 +24,12 @@ router.get('/', getOwners);
 router.get('/:id', getOwnerById);
 router.put('/:id', updateOwner);
 router.post('owners/:id/dogs', createDog);
+router.get('/owners/:id/dogs', getAllDogs);
+router.get('/owners/:id/dogs/:name', getDogById);
+router.put('/owners/:id/dogs/:name', updateDog);
+router.patch('/owners/:id/dogs/:name', updateDogAge);
+router.patch('/owners/:id/dogs/:name', updateDogDiet);
+router.delete('/owners/:id/dogs/:name', deleteDogById);
+router.delete('/owners/:id/dogs', deleteAllDogs);
 
 module.exports = router;
