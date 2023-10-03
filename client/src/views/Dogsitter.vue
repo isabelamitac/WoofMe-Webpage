09753c0d99a9be7a4a15cb52391268f7a523a6d1
Milena Maćkowiak <milena.mackowiak7@gmail.com>
2023-10-03 15:40:32 +0200

    Added Dogsitter and Login files

---
+++ client/src/views/Dogsitter.vue
@@ -0,0 +1,111 @@
+import dogsComponent from '../src/components/Dogs.vue'
+<template>
+  <div class="container">
+    <b-jumbotron
+      header="Dog Sitter Profile"
+      lead="Welcome to your profile section"
+    >
+      <div class="profile-info">
+        <input
+          type="text"
+          placeholder="Your ID"
+          v-model="userInput"
+          @click="getDogsitterInfo"
+          required
+        />
+        <table>
+          <tr>
+            <th>Name:</th>
+            <td v-if="dogsitter != null">{{ dogsitter.name }}</td>
+          </tr>
+          <tr>
+            <th>Location:</th>
+            <td v-if="dogsitter != null">{{ dogsitter.location }}</td>
+          </tr>
+          <tr>
+            <th>Rating:</th>
+            <td v-if="dogsitter != null">{{ dogsitter.rating }}</td>
+          </tr>
+          <tr>
+            <td v-show="loggedIn">Edit profile</td>
+          </tr>
+          <b-button class="btn_message cta-btn" v-on:click="getDogsitterInfo()"
+            >See your profile</b-button
+          >
+        </table>
+      </div>
+    </b-jumbotron>
+    <div class="recent-dogs">
+      <dogsComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'dogsitter',
+  data() {
+    return {
+      dogsitter: '',
+      loggedIn: false,
+      userInput: '',
+      password: '',
+      admin: {},
+      message: 'none'
+    }
+  },
+  methods: {
+    getDogsitterInfo() {
+      const dogsitterID = this.userInput
+      Api.get(`/dogsitters/${dogsitterID}`)
+        .then((res) => {
+          this.dogsitter = res.data
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+
+    createDogsitter() {
+      Api.post('/dogsitters')
+        .then((response) => {
+          this.message = response.data.message
+        })
+        .catch((error) => {
+          this.message = error
+        })
+    }
+  },
+  mounted() {
+    this.getDogsitterInfo()
+  }
+}
+</script>
+
+<style>
+@import url('../assets/styles/style.css');
+.btn_message {
+  margin-top: 10px;
+}
+img {
+  width: 40%;
+  height: 150px;
+}
+.dogsitterContainer {
+  display: grid;
+  grid-template-columns: 50% 25% 25%;
+  height: 150px;
+}
+.dogsitterInfo {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+.recentDogs {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h1>Log in</h1>
+    <div class="login-container">
+      <input type="email" placeholder="Email" v-model="email" required />
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="password"
+        required
+      />
+      <button class="cta-btn">Log in</button>
+      <img />
+      <div class="sign-up-container">
+        <b-jumbotron header="Sign up here!">
+          <p class="urPost">Enter profile details below:</p>
+          <input
+            type="name"
+            placeholder="First name"
+            v-model="adminName"
+            required
+          />
+          <br />
+          <input type="email" placeholder="Email" v-model="email" required />
+          <br />
+          <input type="text" placeholder="City" v-model="location" required />
+          <br />
+          <input
+            type="text"
+            placeholder="Country"
+            v-model="location"
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Choose your password"
+            v-model="password"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Re-write the password"
+            v-model="passwordRep"
+            required
+          />
+        </b-jumbotron>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('../assets/styles/style.css');
+</style>
