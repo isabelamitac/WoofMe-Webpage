239979b6d17cf7011bfc247517582c476e8767d9
mitac <mitac@student.chalmers.se>
2023-10-14 00:13:26 +0200

    Add admin component and model

---
+++ client/src/components/Admin.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <div class="container">
+      <h1>Welcome to your admin page</h1>
+    </div>
+    <br>
+    <button class="second-btn" @click="deleteDogsitters()" >Delete all dogsitters</button> <br/>
+    <br />
+</div>
+  </template>
+
+<script>
+import { Api } from '@/Api'
+
+export default {
+  name: 'admin',
+  data() {
+    return {
+      name: '',
+      email: ''
+    }
+  },
+  methods: {
+    deleteDogsitters() {
+      Api.delete('/dogsitters')
+        .then((res) => {
+          localStorage.clear()
+          console.log(res)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+
+</script>
+
+<style>
+   @import url('../assets/styles/style.css');
+</style>
